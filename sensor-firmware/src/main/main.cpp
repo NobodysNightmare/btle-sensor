da39c4e6cbf93d0be50b3b787b4e3a72ff99305d
Jan Sandbrink <nobodysnightmare@gmail.com>
2019-01-10 16:54:24 +0100

    Actually read data from sensor
    This now fixes the previously untested and
    broken sensor reading code and adds lots of
    verbose logging to the sensor communication (you can imagine
    how development went by looking at the amount of logging needed :D)

--- sensor-firmware/src/main/main.cpp
+++ sensor-firmware/src/main/main.cpp
@@ -1,4 +1,5 @@
 #include "bt.h"
+#include "sensor.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -9,6 +10,12 @@ static const char *tag = "MAIN";
 
 #define ADVERTISE_TIME_SECONDS 5
 #define SENSOR_READ_PERIOD_SECONDS 60
+
+void deinit() {
+    BT::deinit();
+    ESP_LOGI(tag, "Going to sleep...");
+    esp_deep_sleep((SENSOR_READ_PERIOD_SECONDS - ADVERTISE_TIME_SECONDS) * 1000000);
+}
 
 extern "C" void app_main() {
     ESP_LOGI(tag, "Starting up...");
@@ -23,16 +30,26 @@ extern "C" void app_main() {
         ESP_LOGI(tag, "Bluetooth initialized successfully.");
     } else {
         ESP_LOGE(tag, "Bluetooth could not be initialized.");
+        deinit();
         return;
     }
 
-    // TODO: actually read a sensor here...
+    if(!Sensor::init()) {
+        ESP_LOGE(tag, "Sensor could not be initialized.");
+        deinit();
+        return;
+    }
 
-    BT::advertise(215, 650);
+    if(!Sensor::readValues()) {
+        ESP_LOGE(tag, "Sensor could not perform measurement.");
+        deinit();
+        return;
+    }
+
+    ESP_LOGI(tag, "Advertising readings...");
+    BT::advertise(Sensor::getTemperature(), Sensor::getHumidity());
 
     vTaskDelay((1000 * ADVERTISE_TIME_SECONDS) / portTICK_PERIOD_MS);
 
-    ESP_LOGI(tag, "Going to sleep...");
-    BT::deinit();
-    esp_deep_sleep((SENSOR_READ_PERIOD_SECONDS - ADVERTISE_TIME_SECONDS) * 1000000);
+    deinit();
 }
--- sensor-firmware/src/main/sensor.cpp
+++ sensor-firmware/src/main/sensor.cpp
@@ -6,16 +6,16 @@
 #include "math.h"
 
 #include "driver/i2c.h"
+
 #include "esp_log.h"
 static const char *tag = "Sensor";
 
 static i2c_config_t i2c_config;
-#define SENSOR_SDA_PIN GPIO_NUM_7
-#define SENSOR_SCL_PIN GPIO_NUM_8
+#define SENSOR_SDA_PIN GPIO_NUM_22
+#define SENSOR_SCL_PIN GPIO_NUM_23
 #define I2C_PORT I2C_NUM_0
-#define I2C_CLOCK_FREQUENCY_HZ 1000000
-// ~12.5 kB of data at 1 MHz
-#define I2C_TIMEOUT_MILLISECONDS 100
+#define I2C_CLOCK_FREQUENCY_HZ 100000
+#define I2C_TIMEOUT_MILLISECONDS 1000
 
 #define I2C_ERROR_CHECK(call) if((result = call)) { \
                                   ESP_LOGE(tag, "I2C error in call: %s", esp_err_to_name(result)); \
@@ -41,6 +41,8 @@ bool init_i2c() {
 
     esp_err_t result;
     if((result = i2c_param_config(I2C_PORT, &i2c_config))) {
+        // Note: apparently for some misconfiguration (bad pins? no slave connected?) we do not reach
+        // this code, but get a panic... not sure if we can detect this...
         ESP_LOGE(tag, "Configuring I2C failed: %s", esp_err_to_name(result));
         return false;
     }
@@ -54,35 +56,45 @@ bool init_i2c() {
 }
 
 bool Sensor::init() {
+    ESP_LOGI(tag, "Preparing I2C");
     if(!init_i2c()) return false;
-    
+
+    ESP_LOGD(tag, "I2C has been prepared.");
+
     device.dev_id = BME280_I2C_ADDR_PRIM;
     device.intf = BME280_I2C_INTF;
     device.read = user_i2c_read;
     device.write = user_i2c_write;
     device.delay_ms = user_delay_ms;
 
+    ESP_LOGI(tag, "Initializing BME280...");
     int8_t result = bme280_init(&device);
     if(result != BME280_OK) {
         ESP_LOGE(tag, "Sensor initialize failed: %d", result);
         return false;
     }
 
+    ESP_LOGD(tag, "BME280 has been initialized.");
+
     device.settings.osr_h = BME280_OVERSAMPLING_1X;
     device.settings.osr_p = BME280_OVERSAMPLING_1X;
     device.settings.osr_t = BME280_OVERSAMPLING_1X;
     device.settings.filter = BME280_FILTER_COEFF_OFF;
 
+    ESP_LOGI(tag, "Configuring BME280...");
     result = bme280_set_sensor_settings(BME280_OSR_PRESS_SEL | BME280_OSR_TEMP_SEL | BME280_OSR_HUM_SEL | BME280_FILTER_SEL, &device);
     if(result != BME280_OK) {
         ESP_LOGE(tag, "Configuring sensor failed: %d", result);
         return false;
     }
 
+    ESP_LOGD(tag, "BME280 has been configured.");
+
     return true;
 }
 
 bool Sensor::readValues() {
+    ESP_LOGI(tag, "Starting measurement and waiting...");
     int8_t result = bme280_set_sensor_mode(BME280_FORCED_MODE, &device);
     if(result != BME280_OK) {
         ESP_LOGE(tag, "Starting measurement failed: %d", result);
@@ -91,11 +103,14 @@ bool Sensor::readValues() {
 
     device.delay_ms(MEASURE_TIME_MILLISECONDS);
 
+    ESP_LOGI(tag, "Retrieving measurement data...");
     result = bme280_get_sensor_data(BME280_ALL, &sensor_data, &device);
     if(result != BME280_OK) {
         ESP_LOGE(tag, "Fetching sensor data failed: %d", result);
         return false;
     }
+
+    ESP_LOGD(tag, "Measurement received.");
 
     return true;
 }
@@ -113,6 +128,7 @@ uint16_t Sensor::getPressure() {
 }
 
 void user_delay_ms(uint32_t period) {
+    ESP_LOGV(tag, "Waiting for %d ms", period);
     vTaskDelay(period / portTICK_PERIOD_MS);
 }
 
@@ -137,19 +153,28 @@ int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16
      * |------------+---------------------|
      */
 
+    ESP_LOGV(tag, "Preparing cmd link to read data from 0x%x...", dev_id);
     esp_err_t result;
     i2c_cmd_handle_t command = i2c_cmd_link_create();
     I2C_ERROR_CHECK(i2c_master_start(command))
     I2C_ERROR_CHECK(i2c_master_write_byte(command, (dev_id << 1) | I2C_MASTER_WRITE, true))
     I2C_ERROR_CHECK(i2c_master_write_byte(command, reg_addr, true))
-    I2C_ERROR_CHECK(i2c_master_stop(command))
+    I2C_ERROR_CHECK(i2c_master_stop(command)) // documented in the example above, but not in spec sheet
+    I2C_ERROR_CHECK(i2c_master_cmd_begin(I2C_PORT, command, I2C_TIMEOUT_MILLISECONDS / portTICK_RATE_MS))
 
-    I2C_ERROR_CHECK(i2c_master_start(command))
+    ESP_LOGV(tag, "Asking to read...");
+    I2C_ERROR_CHECK(i2c_master_cmd_begin(I2C_PORT, command, I2C_TIMEOUT_MILLISECONDS / portTICK_RATE_MS))
+    ESP_LOGV(tag, "Finished asking to read.");
+
+    command = i2c_cmd_link_create();
+    I2C_ERROR_CHECK(i2c_master_start(command)) // documented in the example above, but not in spec sheet
     I2C_ERROR_CHECK(i2c_master_write_byte(command, (dev_id << 1) | I2C_MASTER_READ, true))
     I2C_ERROR_CHECK(i2c_master_read(command, reg_data, len, I2C_MASTER_LAST_NACK))
     I2C_ERROR_CHECK(i2c_master_stop(command))
 
+    ESP_LOGV(tag, "Beginning to read...");
     I2C_ERROR_CHECK(i2c_master_cmd_begin(I2C_PORT, command, I2C_TIMEOUT_MILLISECONDS / portTICK_RATE_MS))
+    ESP_LOGV(tag, "Finished reading.");
 
     return 0;
 }
@@ -173,6 +198,7 @@ int8_t user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint1
      * |------------+---------------------|
      */
 
+    ESP_LOGV(tag, "Preparing cmd link to write data to 0x%x...", dev_id);
     esp_err_t result;
     i2c_cmd_handle_t command = i2c_cmd_link_create();
     I2C_ERROR_CHECK(i2c_master_start(command))
@@ -181,7 +207,9 @@ int8_t user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint1
     I2C_ERROR_CHECK(i2c_master_write(command, reg_data, len, true))
     I2C_ERROR_CHECK(i2c_master_stop(command))
 
+    ESP_LOGV(tag, "Beginning to write...");
     I2C_ERROR_CHECK(i2c_master_cmd_begin(I2C_PORT, command, I2C_TIMEOUT_MILLISECONDS / portTICK_RATE_MS))
+    ESP_LOGV(tag, "Finished writing.");
 
     return 0;
 }
